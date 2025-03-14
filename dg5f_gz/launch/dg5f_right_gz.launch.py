099fa210fa78bc11c758c2dfedacb68c466a0c8f
Tesollo <99462512+Tesollo-Delto@users.noreply.github.com>
2025-03-14 14:37:12 +0900

    Update dg5f_right_gz.launch.py

--- dg5f_gz/launch/dg5f_right_gz.launch.py
+++ dg5f_gz/launch/dg5f_right_gz.launch.py
@@ -54,7 +54,7 @@ def generate_launch_description():
 
     # Get package paths
     pkg_delto_description = FindPackageShare(
-        "dg_m_description").find("dg_m_description")
+        "dg_description").find("dg_description")
     model_path = os.path.join(pkg_delto_description, "meshes")
 
     # Set Gazebo model path
